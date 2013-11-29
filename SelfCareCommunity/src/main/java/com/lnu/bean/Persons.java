01d33277ca00b5e9abfc11be0efc5637cbb7f7c8
ag222mw <ag222mw@cdcecde4-c95e-44f9-8b9b-2230d9e474ac>
2013-11-29 08:57:06 +0000

    Added class Person to com.lnu.bean, that maps Hibernate with table PEERONS
    git-svn-id: http://vhost8.lnu.se:20098/svn/team2@27 cdcecde4-c95e-44f9-8b9b-2230d9e474ac

---
+++ SelfCareCommunity/src/main/java/com/lnu/bean/Persons.java
@@ -0,0 +1,115 @@
+package com.lnu.bean;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PERSONS")
+public class Persons {
+	
+	@Id
+	@Column(name = "person_id_PK")
+	private Long id;
+	
+	@Column(name = "person_name")
+	private String name;
+	
+	@Column(name = "person_surname")
+	private String surname;
+	
+	@Column(name = "person_birth_date")
+	private Date birthDate;
+	
+	@Column(name = "person_gender")
+	private String gender;
+	
+	@Column(name = "person_health_desease")
+	private String healthDesease;
+	
+	@Column(name = "person_interests")
+	private String interests;
+	
+	@Column(name = "person_profile_picture")
+	private String profilePicture;
+	
+	@Column(name = "person_zip_code")
+	private Integer zipCode;
+	
+	// Getter and Setter methods
+	
+	public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getSurname() {
+    	return surname;
+    }
+    
+    public void setSurname(String surname) {
+    	this.surname = surname;
+    }
+    
+    public Date getBirthDate() {
+    	return birthDate;
+    }
+    
+    public void setBirthDate(Date birthDate) {
+    	this.birthDate = birthDate;
+    }
+    
+    public String getGender() {
+    	return gender;
+    }
+    
+    public void setGender(String gender) {
+    	this.gender = gender;
+    }
+    
+    public String getHealthDesease() {
+    	return healthDesease;
+    }
+    
+    public void setHealthDesease(String healthDesease) {
+    	this.healthDesease = healthDesease;
+    }
+    
+    public String getInterests() {
+    	return interests;
+    }
+    
+    public void setInterests(String interests) {
+    	this.interests = interests;
+    }
+    
+    public String getProfilePicture() {
+    	return profilePicture;
+    }
+    
+    public void setProfilePicture(String profilePicture) {
+    	this.profilePicture = profilePicture;
+    }
+    
+    public Integer getZipCode() {
+    	return zipCode;
+    }
+    
+    public void setZipCode(Integer zipCode) {
+    	this.zipCode = zipCode;
+    }  	
+}

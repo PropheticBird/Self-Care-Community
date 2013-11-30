940394ab3ce79a85fc57842480e2d4ed2c678333
yr222aa <yr222aa@cdcecde4-c95e-44f9-8b9b-2230d9e474ac>
2013-11-30 12:20:53 +0000

    Bringing data from DB for profile. It works in local. email and username are missing
    git-svn-id: http://vhost8.lnu.se:20098/svn/team2@39 cdcecde4-c95e-44f9-8b9b-2230d9e474ac

---
+++ SelfCareCommunity/src/main/webapp/scripts/Data_from_DB_Profile.js
@@ -0,0 +1,41 @@
+function GetUserData () {
+	//console.log(window.location.hostname);
+	var url = window.location.href;
+	var relativePath = "/SelfCareCommunity/service/currentuserdetails";
+	var arr = url.split("/");
+	var hostName = arr[0] + "//" + arr[2];
+	//console.log(result);
+	 //var url = "http://demo.group2.us.to:8080/SelfCareCommunity/service/currentuserdetails";
+	var url = hostName + relativePath ;
+	console.log(url);
+	
+	//url = 'http://localhost:8080/SelfCareCommunity/service/currentuserdetails';
+	  //     http://localhost:8080/SelfCareCommunity/service/currentuserdetails 
+	$.getJSON(url, function(data) {
+		mainfunction(data);
+	});
+	
+	function mainfunction(data) {
+		// getting textbox objects
+		tbUserName = document.getElementById("tbUserName");
+		tbName = document.getElementById("tbName");
+		tbSurName = document.getElementById("tbSurName");
+		tbEmail = document.getElementById("tbEmail");
+		tbDofB = document.getElementById("tbDofB");
+		tbGender = document.getElementById("tbGender");
+		tbZipCode = document.getElementById("tbZipCode");
+		tbInterests = document.getElementById("tbInterests");
+		
+		//adding value to these texboxes
+		tbUserName.value = data.userName;
+		tbName.value = data.name;
+		tbSurName.value = data.surname;
+		tbEmail.value = data.email;
+		tbDofB.value = data.birthdate;
+		tbGender.value = data.gender;
+		tbZipCode.value = data.zipCode;
+		tbInterests.value = data.interests;
+		
+		console.log(data);   
+	}
+}

52f1d8862b6d4ffec608c40a1a8557634c4a2b4b
ip222cb <ip222cb@cdcecde4-c95e-44f9-8b9b-2230d9e474ac>
2013-11-30 10:11:52 +0000

    Changed redirect after registration to proceed on profile page
    git-svn-id: http://vhost8.lnu.se:20098/svn/team2@36 cdcecde4-c95e-44f9-8b9b-2230d9e474ac

--- SelfCareCommunity/src/main/java/com/lnu/controller/LoginController.java
+++ SelfCareCommunity/src/main/java/com/lnu/controller/LoginController.java
@@ -3,8 +3,14 @@ package com.lnu.controller;
 import com.lnu.bean.PersonCredentials;
 import com.lnu.dao.PersonalCredentialsDao;
 import com.lnu.service.UserService;
+import com.lnu.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
 
 /**
  * PersonCredentials: igor
@@ -43,9 +52,20 @@ public class LoginController {
     }
 
     @RequestMapping(value="/register", method = RequestMethod.POST)
-    public String register(@ModelAttribute PersonCredentials user) {
+    public String register(HttpServletRequest request, @ModelAttribute PersonCredentials user) {
         userService.registerUser(user);
-        return "login";
+        login(request,user.getUserName(),user.getPassword());
+        return "redirect:content/profile.html";
+    }
+
+    public void login(HttpServletRequest request, String userName, String password)
+    {
+        Authentication auth= new UsernamePasswordAuthenticationToken(userName, password, Arrays.asList(new SimpleGrantedAuthority(UserServiceImpl.ROLE_USER)));
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(auth);
+        // Create a new session and add the security context.
+        HttpSession session = request.getSession(true);
+        session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
     }
 
 }

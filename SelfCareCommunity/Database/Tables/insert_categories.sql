e51782a88f5df2baff6d8c9de2d3afd49c411b8b
ag222mw <ag222mw@cdcecde4-c95e-44f9-8b9b-2230d9e474ac>
2013-12-06 12:51:24 +0000

    Added script that add categories into the categories table
    git-svn-id: http://vhost8.lnu.se:20098/svn/team2@66 cdcecde4-c95e-44f9-8b9b-2230d9e474ac

---
+++ SelfCareCommunity/Database/Tables/insert_categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (Display_Name)
+VALUES ('Coronary artery disease'), 
+		('Myocardial infarction'), 
+			('Heart failure'),
+				('Myocarditis'),
+					('Heart rhythm disorders'),
+						('Food'),
+							('Sport');

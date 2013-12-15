1dc95221da533c507cc790effef84f0332c2818b
yr222aa <yr222aa@cdcecde4-c95e-44f9-8b9b-2230d9e474ac>
2013-12-15 12:00:14 +0000

    Pagination fully working, finally!
    git-svn-id: http://vhost8.lnu.se:20098/svn/team2@121 cdcecde4-c95e-44f9-8b9b-2230d9e474ac

--- SelfCareCommunity/src/main/webapp/scripts/forum.js
+++ SelfCareCommunity/src/main/webapp/scripts/forum.js
@@ -1,10 +1,12 @@
 var currentPage=1;
 var idCat = 0; 
+var num_pages=0;
 $(document).ready(function() {
 	
 	var prmstr = window.location.search.substr(1);
     var prmarr = prmstr.split ("&");
     var params = {};
+    
 	
     for ( var i = 0; i < prmarr.length; i++) {
         var tmparr = prmarr[i].split("=");
@@ -16,28 +18,30 @@ $(document).ready(function() {
 	idCat= params['categoryId']; // used for getting the category id from URL
 	
 	var threadCount=params['threadCount'];	
-	var n=parseInt(threadCount/10)+1;
+	num_pages=parseInt(threadCount/10)+1; // it calculates the numbe of pages we will have
 	
 	prePage=1;
 	
-	$("#pagePre").parent().attr("class","active");
-	$("#page1").parent().attr("class","active");
+	$(".pagePre").parent().attr("class","active");
+	$(".page1").parent().attr("class","active");
 	
-	if(n>1)	{
-		element = '<li><a onclick="page(this);return false;"id="pagePre">&laquo</a></li>';
-		$("#page").append(element);
-		for(var i=1;i<=n;i++){
-			
-			element = '<li><a onclick="page(this);return false;"id="page'+i+'">'+i+'</a></li>';
-			$("#page").append(element);
+	if(num_pages>1)	{
+		element = '<li><a onclick="page(this);return false;"class="pagePre">&laquo</a></li>';
+		$(".page").append(element);
+		element = '<li><a onclick="page(this);return false;"class="page1"><span class="page1span label label-info">1</span></a></li>';
+		$(".page").append(element);
+		for(var i=2;i<=num_pages;i++){ 
+			clas='page'+i+'span';
+			element = '<li><a onclick="page(this);return false;"class="page'+i+'"><span class='+clas+'>'+i+'</span></a></li>';
+			$(".page").append(element);
 		}
-		element = '<li><a onclick="page(this);return false;"id="pageNext">&raquo;</a></li>';
-		$("#page").append(element);
+		element = '<li><a onclick="page(this);return false;"class="pageNext">&raquo;</a></li>';
+		$(".page").append(element);
 					
 	}
 	else{
-		element = '<li><a onclick="page(this);return false;"id="page1">1</a></li>';
-		$("#page").append(element);
+		element = '<li><a onclick="page(this);return false;"class="page1">1</a></li>';
+		$(".page").append(element);
 		
 	}
 	
@@ -88,36 +92,87 @@ $(document).ready(function() {
 });
 
 function page(item){
+	prePage=currentPage;
 	
-	if (item.id=='pageNext'){
-		getThreads(currentPage+1);
+	if (item.className=='pageNext'){
 		currentPage++;
-    		
+		getThreads(currentPage);
+			
 	}
-	if (item.id=='pagePre'){
-		getThreads(currentPage-1);
+	if (item.className=='pagePre'){
 		currentPage--;
+		getThreads(currentPage);
+		
 	}
 	
-	if ($.isNumeric(item.innerHTML)){
-		currentPage=parseInt(item.innerHTML);
-		getThreads(item.innerHTML);
-	}
-	
-	$("#page"+prePage).parent().attr("class","");
-	$("#page"+currentPage).parent().attr("class","active");
-	
-
-	if(currentPage==1)
-		$("#pagePre").parent().attr("class","active");
-	else
-	{
-		$("#pagePre").parent().attr("class","");
-		$("#pagePre").attr("href","/SelfCareCommunity/service/category/"+idCat+"/threads/page/"+(currentPage-1));
-		$("#pageNext").attr("href","/SelfCareCommunity/service/category/"+idCat+"/threads/page/"+(currentPage+1));
-	}
+	if ($.isNumeric(item.innerText)){
+		currentPage=parseInt(item.innerText);
+		getThreads(item.innerText);
+	} 
+	changeNums(prePage, currentPage);
 }
 	 	    	
+function changeNums(tPrePage, tcPage) {
+	
+	prePage = tPrePage;	
+	currentPage=tcPage;
+
+
+	clas='.page'+prePage+'span';
+	
+	$(".page"+prePage).parent().attr("class","");
+	$(clas).attr('class','page'+prePage+'span');
+	
+	clas='.page'+currentPage+'span';
+	
+	$(".page"+currentPage).parent().attr("class","active");
+	$(clas).attr("class","page"+currentPage+"span label label-info");
+
+	if(currentPage==1)
+		$(".pagePre").parent().attr("class","active");
+	else
+	{
+		$(".pagePre").parent().attr("class","");
+		$(".pagePre").attr("href","/SelfCareCommunity/service/category/"+idCat+"/threads/page/"+(currentPage-1));
+		$(".pageNext").attr("href","/SelfCareCommunity/service/category/"+idCat+"/threads/page/"+(currentPage+1));
+	}
+}
+
+function paginationManagement(last, current){ // this function will handle the visualization of the elements
+	if (last==true){
+		$(".pageNext").hide();
+	} else {
+		$(".pageNext").show();
+	} 
+	if (current==1){
+		$(".pagePre").hide();
+	} else {
+		$(".pagePre").show();
+	}
+	
+	//in case the number of pages is bigger than 6...
+	if (num_pages > 4) {
+		$(".tempAfter").remove();	
+		$(".tempBefore").remove();	
+		for (i= 3; i<=(num_pages -2); i++){
+				$(".page" + i).hide();	
+			
+		}
+		if (current < (num_pages - 2) -1) {
+			$(".page"+(current+1)).after("<span class='tempAfter'>. . .</span>");
+		}	
+		if (current > 4) {
+			$(".page"+(current-1)).before("<span class='tempBefore'>. . .</span>");
+		}
+		for (i= 1; i<=num_pages; i++){
+			if (( i == current)||  (i == current +1) || (i == current -1)) {
+			
+				$(".page" + i).show();	
+			}
+		}
+	}
+}
+
 function getThreads(pageNum){
 	    
 	var url="/SelfCareCommunity/service/category/"+idCat+"/threads/page/"+pageNum;
@@ -129,18 +184,8 @@ function getThreads(pageNum){
 		success: function(result){    
 			lastPage = result.last;
 			data=result.data;
-			if (lastPage==true){
-				$("#pageNext").hide();
-			} else {
-				$("#pageNext").show();
-					
-			} 
-			if (pageNum==1){
-				$("#pagePre").hide();
-			} else {
-				$("#pagePre").show();
-					
-			}
+			
+			paginationManagement(lastPage,  currentPage);
 			$("#threadList").html("");
 
 			for(var i=0;i<data.length;i++){
